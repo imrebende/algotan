84c96668d25200a05317f5bdef1b9738275d2e24
Bende Imre <imre.bende@smartx.eu>
2016-07-15 13:37:07 +0200

    uglify

---
+++ uglify.sh
@@ -0,0 +1,10 @@
+uglifyjs -o js/programozasitetelek.min.js --compress --mange -- js/programozasitetelek.min.js
+uglifyjs -o js/osszetettprogtetelek.min.js --compress --mange -- js/osszetettprogtetelek.min.js
+
+uglifyjs -o js/rendezesek.min.js --compress --mange -- js/rendezesek.min.js
+
+uglifyjs -o js/esemenyek.min.js --compress --mange -- js/esemenyek.min.js
+uglifyjs -o js/eljarasok.min.js --compress --mange -- js/eljarasok.min.js
+
+uglifyjs -o js/verem.min.js --compress --mange -- js/verem.js
+uglifyjs -o js/sor.min.js --compress --mange -- js/sor.js
