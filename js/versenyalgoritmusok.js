9ed8b7b6c39682083a293ec4c142aa811871a320
Imre Bende <imre.bende@smartx.eu>
2017-07-15 15:36:03 +0200

    geo init, hadlebar linux fix

--- js/versenyalgoritmusok.js
+++ js/versenyalgoritmusok.js
@@ -1,3 +1,33 @@
+$( function() {
+	for(var i = 0; i < 3; i++){
+		sliderGenerator($(".slider").length);
+	}
+} );
+
+function ujSlider(){
+	sliderGenerator($(".slider").length);
+}
+
+function sliderGenerator(num){
+	$("#sliders").append( "<div class='slider-container' id='slider-" + num + "'><div class='sliderCimke'>" + (num + 1) + ". ( <span id='amount" + num + "'></span> ) <button class='btn btn-xs btn-danger' id='sliderTorles" + num + "' onclick='sliderTorles(" + num + ")'><span class='glyphicon glyphicon-remove'></span>&nbsp;</button></div> <div id='slider-range" + num + "' class='slider'></div></div>" );
+
+	$( "#slider-range" + num ).slider({
+		range: true,
+		min: 0,
+		max: 500,
+		values: [ 75, 300 ],
+		slide: function( event, ui ) {
+			$( "#amount" + num ).html( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+		}
+	});
+	$( "#amount" + num).html( $( "#slider-range" + num ).slider( "values", 0 ) +
+		" - " + $( "#slider-range" + num ).slider( "values", 1 ) );
+}
+
+function sliderTorles(num){
+	$( "#slider-" + num ).remove();
+}
+
 function mohoStart(){
 	fuggvenyElottiInit();
 	var t = [];
@@ -13,9 +43,7 @@ function mohoStart(){
 }
 
 function moho(tomb){
-	//console.log(tomb);
 	tomb.sort(function(a, b){return a.veg - b.veg});
-	//console.log(tomb);
 	
 	var db = 0;
 	var eredmeny = [];
@@ -24,9 +52,8 @@ function moho(tomb){
 		if(utolsoVege < tomb[i].kezd){
 			utolsoVege = tomb[i].veg - 1;
 			eredmeny.push(utolsoVege);
-			valtozokKiirasa({nev: "eredmeny", ertek: eredmeny}, "");
 		}
+		valtozokKiirasa({nev: "eredmeny", ertek: eredmeny}, "");
 	}
 	megjelenitesInit();
-	//console.log(eredmeny);
 }
